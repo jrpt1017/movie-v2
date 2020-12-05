1b788bb53b0deba96ac12d188bbd5514b7455997
jonnelrodjiptolosa <jonnelrodji.tolosa@hcl.com>
2020-12-06 07:19:19 +0800

    installed dependencies

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Typography } from '@material-ui/core';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Typography>Learn Reacts</Typography>
+      </header>
+    </div >
+  );
+}
+
+export default App;
